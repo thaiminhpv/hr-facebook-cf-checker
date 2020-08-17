ec0156a1f346f0f509faa5975c1dac7922972cea
thaiminhpv <thaiminhpv@gmail.com>
2020-08-17 17:17:51 +0700

    milestones

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log('It work!')
+
+/*
+ * TODO:
+ *  1. create Google Spreadsheet format
+ *  2. get Facebook API query Post
+ *  3. get list comments
+ *  4. get list user comments (use Set (no duplicate user))
+ *  5. get Google Spreadsheet API
+ *  6. write code that who has been checked (API Google Spreadsheet side) with timestamp
+ *  7. build CUI with 2 parameter: post ID and sheet ID
+ *  8. get Facebook API post trigger automatically query update data every 2h
+ *  9. get Facebook API post trigger automatically insert 5 comments auto: HR, Medes, PR,...
+ *  10. Google Spreadsheet conditional formatting color base on the difference with deadline
+ *  11. push to Firebase
+ *  12. Count as score
+ *  13. Draw charts
+ *  14. Build Web service with frontend (+Express,...)
+ */
