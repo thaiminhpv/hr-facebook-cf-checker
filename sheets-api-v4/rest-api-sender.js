e06e4419420eb881e98f30e002d9ac002b8d5756
thaiminhpv <thaiminhpv@gmail.com>
2020-08-18 20:16:05 +0700

    call Facebook API and call Spreadsheets API

--- sheets-api-v4/rest-api-sender.js
+++ sheets-api-v4/rest-api-sender.js
@@ -1,34 +1,108 @@
-const querystring = require('querystring');
-const https = require('https');
+const axios = require('axios');
+const {spreadsheets, facebook} = require('./config.json');
 
-const { sheets_id, range, sheets_name, API_key } = require('./config.json');
+const {sheets_id, range, sheets_name, API_key} = spreadsheets
+const {access_token, group_id} = facebook
 
-// const URL = `https://sheets.googleapis.com/v4/spreadsheets/${sheets_id}/values/${sheets_name}!${range}?key=${API_key}`;
-const URL = `https://sheets.googleapis.com/v4/spreadsheets/${sheets_id}/values/${sheets_name}!${range}`
-// console.log(URL);
 
-const parameters = {
-    key: API_key
+//    "group_id": "598883897329508"155017612502508
+
+// console.log(getCommentsFromPostID("155017612502508_155026279168308"))
+// console.log(getReplyComments(155051485832454))
+
+getGroupFirstPostID().then(postID => {
+    return getCommentsFromPostID(postID)
+}).then((listIDCallCf) => {
+    let userCf_ed = []
+    listIDCallCf.forEach((department) => {
+        userCf_ed.push(getReplyComments(department))
+    })
+    return userCf_ed
+}).then(response => {
+    console.log(response)
+    console.log("Done!");
+})
+
+// callSpreadsheetsAPI();
+
+async function getReplyComments(commentID) {
+    console.log("getting reply comments from comment " + commentID)
+    const URL = `https://graph.facebook.com/${commentID}/comments`
+    return await axios.get(URL, {
+        params: {
+            access_token: access_token
+        }
+    })
+        .then(response => {
+            // return list of user commented
+            let user = []
+            response.data.data.forEach((reply_comment) => {
+                // push whole object with @Name just for debugging purpose
+                user.push(reply_comment.from)
+            })
+            console.log(response.data.data);
+            return user
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
 
-const link = `${URL}?key=${API_key}`;
-console.log(link);
-// send request
-const request = https.request(link, (response) => {
-    // response from server
-    console.log("3");
-    console.log(response.body);
-});
-console.log("2");
+async function getCommentsFromPostID(post_id) {
+    console.log("getting comments from post " + post_id)
+    const URL = `https://graph.facebook.com/${post_id}/comments`
+    return await axios.get(URL, {
+        params: {
+            access_token: access_token
+        }
+    })
+        .then(response => {
+            // return list of IDs of comments like PR,HR,Medes,...
+            // FIXME: can't have permission to get other users'id except myself's id
+            let list_id = []
+            response.data.data.forEach((department) => {
+                list_id.push(department.from)
+            })
+            console.log(response.data.data);
+            return list_id
+            console.log(response.data.data[0]);
+            return response.data.data[0].id
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
 
-// In case error occurs while sending request
-request.on('error', (error) => {
-    console.log("4");
-    console.log(error.message);
-});
+async function getGroupFirstPostID() {
+    const URL = `https://graph.facebook.com/${group_id}/feed`
+    return await axios.get(URL, {
+        params: {
+            access_token: access_token,
+            // limit: 1
+        }
+    })
+        .then(response => {
+            console.log(response.data.data[3]); //0
+            return response.data.data[3].id //0
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
 
-request.end();
+function callSpreadsheetsAPI() {
+    const spreadsheetsURL = `https://sheets.googleapis.com/v4/spreadsheets/${sheets_id}/values/${sheets_name}!${range}`;
+    axios.get(spreadsheetsURL, {
+        params: {
+            key: API_key,
+            majorDimension: "COLUMNS"
+        }
+    })
+        .then(response => {
+            console.log(response.data.values);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
 
-// https.get(URL, (res) => {
-//     console.log(res.body);
-// })
