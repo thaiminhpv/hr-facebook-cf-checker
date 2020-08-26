36ab29486994cfb707403c8ad7fccb5a54b29723
thaiminhpv <thaiminhpv@gmail.com>
2020-08-26 13:36:16 +0700

    receive react-post-data

--- firebase-server-express/firebase/functions/resources/browser-inject-js/injector/get-like.js
+++ firebase-server-express/firebase/functions/resources/browser-inject-js/injector/get-like.js
@@ -1,3 +1,36 @@
-let shareLink = 'https://m.facebook.com/ufi/reaction/profile/browser/?ft_ent_identifier='
-let postID = window.location.href.match(/\/(\d+)/)[1]
-window.open(shareLink + postID, '__blank')
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function get(URL, data, mode) {
+    return fetch(`${URL}?mode=${mode}&data=${JSON.stringify(data)}`)
+}
+
+function atClient() {
+    let shareLink = 'https://m.facebook.com/ufi/reaction/profile/browser/?ft_ent_identifier='
+    let postID = window.location.href.match(/\/(\d+)/)[1]
+    window.open(shareLink + postID, '__blank')
+}
+
+async function openMinimized() {
+    while ([...document.getElementsByClassName('touchable primary')].filter((e) => e.innerText === "See More…")[0] !== undefined) {
+        [...document.getElementsByClassName('touchable primary')].filter((e) => e.innerText === "See More…")[0].click();
+        await sleep(3000);
+    }
+}
+
+async function atPopUp() {
+    let url = 'http://localhost:5000'
+// let url = 'https://facebook-check-c-1597716165009.web.app'
+    const route = '/facebook-endpoint'
+    const mode = 'react'
+    const link = url + route
+
+    await openMinimized()
+    console.log("all opened, sending data...")
+    let userShare = [...document.querySelectorAll('._4mo')].map(e => e.innerText)
+    console.log(userShare)
+    get(link, userShare, mode).then(response => {
+        console.log(response)
+    }).catch(error => console.error(error))
+}
