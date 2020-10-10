650e6fbd544990d0fc35888ac6f25ba16d08f431
MinhPortable <thaiminhpv@gmail.com>
2020-10-10 22:18:32 +0700

    change share route and optimize cmt

--- firebase-server-express/firebase/functions/resources/browser-inject-js/injector/get-share.js
+++ firebase-server-express/firebase/functions/resources/browser-inject-js/injector/get-share.js
@@ -1,35 +1,48 @@
+function countFrequency(arr) {
+    let counts = {};
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+    let out = []
+    for (const countsKey in counts) {
+        out.push([countsKey, counts[countsKey]])
+    }
+    return out;
+}
+
 function get(URL, data, mode) {
     return fetch(`${URL}?mode=${mode}&data=${JSON.stringify(data)}`)
 }
 
-function atClient() {
-    let shareLink = 'https://m.facebook.com/browse/shares?id='
-    let currentURL = window.location.href
-    let postID;
-    if (currentURL.match(/story_fbid=(\d+)/)) {
-        postID = currentURL.match(/story_fbid=(\d+)/)[1]
-    } else {
-        postID = currentURL.match(/\/(\d+)/)[1];
-    }
-    window.open(shareLink + postID, '__blank')
+async function openAllMinimized(){
+    console.log("expanding...");
+    //TODO: paste merge code here
+    return sleep(parseInt(prompt("delay?", 300)) * 1000);
 }
 
-function atPopUp() {
+function getAllSharedUser() {
+    return countFrequency([...document/*TODO: insert class count here*/].map((e) => e.innerText))
+}
+
+async function atPopUp() {
     // let url = 'http://localhost:5000'
     let url = 'https://facebook-check-c-1597716165009.web.app'
     const route = '/facebook-endpoint'
     const mode = 'share'
     const link = url + route
-    let userShare = [...document.querySelectorAll('._4mo')].map(e => e.innerText)
+    await openAllMinimized();
+    console.log("all opened, sending data...");
+
+    let userShare = getAllSharedUser();
+    console.log(userShare);
+
+    //TODO: get the key only
+
     get(link, userShare, mode).then(response => {
         console.log(response)
         return response
     }).catch(error => console.error(error))
 }
 
-// eslint-disable-next-line no-alert
-if (parseInt(prompt(`Origin or Pop-up? 1:Origin 2:Pop-up`)) === 1) {
-    atClient()
-} else {
-    atPopUp().then(response => console.log(response)).catch(error => console.error(error))
-}
+atPopUp().then(response => console.log(response)).catch(error => console.error(error))
